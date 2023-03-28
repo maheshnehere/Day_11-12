32801bfcc6d188822cf123b53396e36e83f4659a
maheshnehere <maheshnehere93@gmail.com>
2023-03-28 20:17:59 +0530

    Report Stock management

--- StockManagement/Main.java
+++ StockManagement/Main.java
@@ -1,26 +1,42 @@
 package com.oops_problems;
 
-import java.util.ArrayList;
-import java.util.Scanner;
 
-public class Main {
-    public static ArrayList<String> portfolio = new ArrayList<>();
+public class StockReport {
+    private String stockName;
+    private String numberOfShare;
+    private String sharePrice;
 
-    public static void addmethod() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Stock Name = ");
-        String stockName = sc.next();
-        System.out.println("Enter Number of shares = ");
-        String numberOfShares = sc.next();
-        System.out.println("Enter share price = ");
-        String sharePrice = sc.next();
-
-        StockReport report = new StockReport(stockName, numberOfShares, sharePrice);
-        System.out.println(report.toString());
-        portfolio.add(report.toString());
+    public StockReport(String stockName, String numberOfShare, String sharePrice) {
+        this.stockName = stockName;
+        this.numberOfShare = numberOfShare;
+        this.sharePrice = sharePrice;
     }
 
-    public static void main(String[] args) {
-        addmethod();
+    public String getStockName() {
+        return stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    public String getNumberOfShare() {
+        return numberOfShare;
+    }
+
+    public void setNumberOfShare(String numberOfShare) {
+        this.numberOfShare = numberOfShare;
+    }
+
+    public String getSharePrice() {
+        return sharePrice;
+    }
+
+    public void setSharePrice(String sharePrice) {
+        this.sharePrice = sharePrice;
+    }
+
+    public String toString(){
+        return "\nStock name= "+stockName+"\nNumber of shares = "+numberOfShare+"\nshare Price = "+sharePrice;
     }
 }
